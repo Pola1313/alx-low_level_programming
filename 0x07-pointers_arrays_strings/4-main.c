8a11a1952c44fefcf19606666ef692baa1c6a3f6
Pola1313 <paula.magdy1@gmail.com>
2023-07-31 12:08:18 -0700

    s

---
+++ 0x07-pointers_arrays_strings/4-main.c
@@ -0,0 +1,21 @@
+FYI: The standard library provides a similar function: strpbrk. Run man strpbrk to learn more.
+
+julien@ubuntu:~/0x07$ cat 4-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strpbrk(s, f);
+    printf("%s\n", t);
+    return (0);
+}
